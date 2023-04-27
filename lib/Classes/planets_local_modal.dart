39dcedec8d47d0d696dc3786e0efa748af2277cf
Deilan Raj Victor Jonathan <deilanraj9799@gmail.com>
2023-04-28 00:51:46 +0800

    added the name property to the planet modal

--- lib/Classes/planets_local_modal.dart
+++ lib/Classes/planets_local_modal.dart
@@ -1,14 +1,17 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class PlanetsLocalModal {
+  String? name;
   String? description;
   String? imagePath;
   PlanetsLocalModal({
+    this.name,
     this.description,
     this.imagePath,
   });
 
   PlanetsLocalModal.fromJson(Map<String, dynamic> json) {
-    description = json['descripiton'];
+    name = json['name'];
+    description = json['description'];
     imagePath = json['imagePath'];
   }
 }
