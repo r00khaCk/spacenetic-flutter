74c441ddb36e22046decba958d42b2a548d11eed
r00khaCk <deilanraj9799@gmail.com>
2023-04-27 11:44:05 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# spacenetic_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/spacenetic_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.spacenetic_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
