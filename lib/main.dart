1b5a4a604bd2385a16595a2d4a9d7bec000aa2fa
r00khaCk <deilanraj9799@gmail.com>
2023-04-28 18:05:52 +0800

    added MaterialApp() in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,16 @@ import 'package:flutter/material.dart';
 import 'UI/homepage.dart';
 
 void main() {
-  runApp(const HomePage());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
 }
