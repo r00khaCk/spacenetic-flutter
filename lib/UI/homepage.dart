df730e15d6d93424060b85fbc8754557a20c24eb
r00khaCk <deilanraj9799@gmail.com>
2023-04-28 18:03:52 +0800

    added navigation to PlanetDetailsPage

--- lib/UI/homepage.dart
+++ lib/UI/homepage.dart
@@ -4,6 +4,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:spacenetic_flutter/Classes/planets_local_modal.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:spacenetic_flutter/UI/planet_description_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,107 +14,110 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<PlanetsLocalModal> planetImg = [];
+  List<PlanetsLocalModal> planetObject = [];
   Future<String> loadAsset() async {
     return await rootBundle.loadString('assets/planet_details.json');
   }
 
   @override
   void initState() {
-    super.initState();
-
-    void fetchPlanetsInfo() async {
-      String jsonPlanetInfo = await loadAsset();
-
-      Map<String, dynamic> jsonObj = jsonDecode(jsonPlanetInfo);
-
-      List<dynamic> jsonObjList = jsonObj['planet_details'];
-      setState(() {
-        planetImg =
-            jsonObjList.map((e) => PlanetsLocalModal.fromJson(e)).toList();
-      });
-    }
-
     fetchPlanetsInfo();
+
+    super.initState();
+  }
+
+  Future<void> fetchPlanetsInfo() async {
+    String jsonPlanetInfo = await loadAsset();
+
+    Map<String, dynamic> jsonObj = jsonDecode(jsonPlanetInfo);
+
+    List<dynamic> jsonObjList = jsonObj['planet_details'];
+    setState(() {
+      planetObject =
+          jsonObjList.map((e) => PlanetsLocalModal.fromJson(e)).toList();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/images/main-bg.png"),
-                  fit: BoxFit.cover)),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 10.0, right: 10, top: 50),
-                child: Row(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(right: 20, top: 0),
-                      child: SizedBox(
-                          height: 35,
-                          child: Icon(
-                            Icons.menu,
-                            size: 35,
-                            color: Colors.white,
-                          )),
-                    ),
-                    const Spacer(),
-                    Container(
-                        width: 150,
-                        height: 80,
-                        padding:
-                            const EdgeInsets.only(left: 20, top: 20, bottom: 5),
-                        child: const Text(
-                            style: TextStyle(
-                                fontSize: 30,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                letterSpacing: 1),
-                            "Hi User,")),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 70.0),
-                child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/main-bg.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0, right: 10, top: 50),
+              child: Row(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(right: 20, top: 0),
+                    child: SizedBox(
+                        height: 35,
+                        child: Icon(
+                          Icons.menu,
+                          size: 35,
+                          color: Colors.white,
+                        )),
+                  ),
+                  const Spacer(),
                   Container(
-                      //margin: EdgeInsets.only(right: 10),
-                      width: 320,
-                      height: 30,
-                      padding: const EdgeInsets.only(
-                        left: 75,
-                      ),
+                      width: 150,
+                      height: 80,
+                      padding:
+                          const EdgeInsets.only(left: 20, top: 20, bottom: 5),
                       child: const Text(
                           style: TextStyle(
-                            fontSize: 22,
-                            color: Colors.white,
-                          ),
-                          "Let's Explore Our Solar System!")),
-                ]),
+                              fontSize: 30,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 1),
+                          "Hi User,")),
+                ],
               ),
-              Expanded(
-                child: Center(
-                  child: CarouselSlider.builder(
-                    options: CarouselOptions(
-                        height: 300,
-                        enlargeCenterPage: true,
-                        enlargeFactor: 0.6),
-                    itemCount: planetImg.length,
-                    itemBuilder: (context, index, realIndex) {
-                      final planetImage = planetImg[index].imagePath ?? "";
-                      final planetName = planetImg[index].name ?? "";
-                      return buildImage(planetImage, planetName, index);
-                    },
-                  ),
-                ),
-              )
-            ],
-          ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 70.0),
+              child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                Container(
+                    //margin: EdgeInsets.only(right: 10),
+                    width: 320,
+                    height: 30,
+                    padding: const EdgeInsets.only(
+                      left: 75,
+                    ),
+                    child: const Text(
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                        ),
+                        "Let's Explore Our Solar System!")),
+              ]),
+            ),
+            planetObject.isEmpty
+                ? const CircularProgressIndicator()
+                : Expanded(
+                    child: Center(
+                      child: CarouselSlider.builder(
+                        options: CarouselOptions(
+                            height: 300,
+                            enlargeCenterPage: true,
+                            enlargeFactor: 0.6),
+                        itemCount: planetObject.length,
+                        itemBuilder: (context, index, realIndex) {
+                          final planetImage =
+                              planetObject[index].imagePath ?? "";
+                          final planetName = planetObject[index].name ?? "";
+                          return buildImage(planetImage, planetName, index);
+                        },
+                      ),
+                    ),
+                  )
+          ],
         ),
       ),
     );
@@ -125,14 +129,22 @@ class _HomePageState extends State<HomePage> {
         // color: Colors.grey,
         children: [
           Expanded(
-            child: Image.asset(
-              planetImage,
-              fit: BoxFit.cover,
+            child: InkWell(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) =>
+                      PlanetDetailsPage(planetsLocalModal: planetObject[index]),
+                ));
+              },
+              child: Image.asset(
+                planetImage,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
           Text(
             planetName,
-            style: TextStyle(
+            style: const TextStyle(
               fontSize: 30,
               color: Colors.white,
             ),
