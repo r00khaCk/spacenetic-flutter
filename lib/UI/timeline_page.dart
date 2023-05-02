63d9419e25531463625d9757c826b6df541d6595
r00khaCk <deilanraj9799@gmail.com>
2023-05-02 18:08:10 +0800

    created the timeline page

---
+++ lib/UI/timeline_page.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:timeline_list/timeline.dart';
+import 'package:timeline_list/timeline_model.dart';
+import '../Classes/timeline/space_event.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
+
+class TimelineWidget extends StatefulWidget {
+  const TimelineWidget({super.key});
+
+  @override
+  State<TimelineWidget> createState() => _TimelineWidgetState();
+}
+
+class _TimelineWidgetState extends State<TimelineWidget> {
+  final PageController pageController =
+      PageController(initialPage: 1, keepPage: true);
+  int pageIx = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // body: PageView(
+      //   onPageChanged: (i) => setState(() => pageIx = i),
+      //   controller: pageController,
+      //   children: pages,
+      // ),
+      body: Container(
+          child: timelineModel(TimelinePosition.Center),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/main-bg.png"),
+              fit: BoxFit.cover,
+            ),
+          )),
+    );
+  }
+
+  timelineModel(TimelinePosition position) => Timeline.builder(
+        lineColor: Colors.white,
+        itemBuilder: centerTimeBuilder,
+        itemCount: spaceEvents.length,
+        physics: position == TimelinePosition.Center
+            ? ClampingScrollPhysics()
+            : BouncingScrollPhysics(),
+        position: position,
+      );
+
+  TimelineModel centerTimeBuilder(BuildContext context, int i) {
+    final spaceEvent = spaceEvents[i];
+    final textTheme = Theme.of(context).textTheme;
+    return TimelineModel(
+        Card(
+          margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+          clipBehavior: Clip.antiAlias,
+          child: Padding(
+            padding: EdgeInsets.all(5.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Image.asset(
+                  spaceEvent.eventImage,
+                  height: 150,
+                  width: 150,
+                ),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                Text(spaceEvent.year, style: textTheme.titleMedium),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                Text(
+                  spaceEvent.eventDescription,
+                  style: textTheme.bodySmall,
+                ),
+                const SizedBox(
+                  height: 8.0,
+                )
+              ],
+            ),
+          ),
+        ),
+        position:
+            i % 2 == 0 ? TimelineItemPosition.right : TimelineItemPosition.left,
+        isFirst: i == 0,
+        isLast: i == spaceEvents.length,
+        iconBackground: spaceEvent.iconbackground,
+        icon: spaceEvent.icon);
+  }
+}
