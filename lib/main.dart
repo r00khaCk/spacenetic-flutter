76d81783ff4c6d3ea359a9479e9a81ac96b07518
wenfayeee <munnfayee@gmail.com>
2023-05-02 17:32:11 +0800

    Initialized app with Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:spacenetic_flutter/firebase_options.dart';
 
 import 'UI/homepage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
