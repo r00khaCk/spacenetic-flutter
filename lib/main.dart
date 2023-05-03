b850bbaf42e26dc1880a2c84f6b321ef0945126a
Deilan Raj Victor Jonathan <deilanraj9799@gmail.com>
2023-05-04 02:56:19 +0800

    no changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+// import 'package:spacenetic_flutter/Functions/fetch_planetAPI.dart';
+// import 'package:spacenetic_flutter/StateManagement/api_cubit/cubit/planet_api_cubit.dart';
+import 'package:spacenetic_flutter/UI/homepage.dart';
 import 'package:spacenetic_flutter/UI/login_page.dart';
 import 'package:spacenetic_flutter/UI/signup_page.dart';
 import 'package:spacenetic_flutter/firebase_options.dart';
 import 'package:spacenetic_flutter/UI/timeline_page.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,9 +24,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
         // home: SignUpPage(),
-        home: LoginPage());
-      //home: HomePage(),
-      //home: TimelineWidget(),
-    );
+        //home: LoginPage());
+        home: HomePage());
+    //home: TimelineWidget(),
   }
 }
