73b56bd01a250a0e56ee77c1f1d0af3e22f0124c
wenfayeee <munnfayee@gmail.com>
2023-05-04 12:19:50 +0800

    Resolving conflicts before pulling from upstream

--- lib/Functions/fetch_potdAPI.dart
+++ lib/Functions/fetch_potdAPI.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:spacenetic_flutter/Classes/login_modal.dart';
 
 class FetchPotdAPI {
   String nasaAPIKey = 'isfGWgCsBFhgmdK5e58FO36owILPFGgbPcDMgx1B';
