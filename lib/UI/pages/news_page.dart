824892b17e149e2620ba1b74002f1366f6ebd312
Deilan Raj Victor Jonathan <deilanraj9799@gmail.com>
2023-05-04 18:08:53 +0800

    removed the BlocProvider and moved it swipe_view

--- lib/UI/pages/news_page.dart
+++ lib/UI/pages/news_page.dart
@@ -8,20 +8,6 @@ import 'package:spacenetic_flutter/Functions/fetch_newsAPI.dart';
 import 'package:spacenetic_flutter/StateManagement/api_cubit/cubit/news_api_cubit.dart';
 import 'package:spacenetic_flutter/UI/widgets/frostedglass.dart';
 
-class DisplayNews extends StatelessWidget {
-  const DisplayNews({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => NewsApiCubit(
-        FetchNewsAPI(),
-      ),
-      child: const SpaceNews(),
-    );
-  }
-}
-
 class SpaceNews extends StatefulWidget {
   const SpaceNews({super.key});
 
@@ -44,9 +30,12 @@ class _SpaceNewsState extends State<SpaceNews> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor: Colors.transparent,
       appBar: AppBar(
-        elevation: 0,
-        title: const Text("News"),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        title: Center(child: Text("News")),
       ),
       body: Container(
         decoration: BoxDecoration(
@@ -68,7 +57,7 @@ class _SpaceNewsState extends State<SpaceNews> {
             } else if (state is ResponseNewsState) {
               List<NewsModal> news = state.newsInfo;
               return ListView.builder(
-                padding: const EdgeInsets.all(15.0),
+                padding: const EdgeInsets.only(top: 100),
                 itemCount: news.length,
                 itemBuilder: ((context, index) {
                   final articles = news[index];
@@ -83,7 +72,7 @@ class _SpaceNewsState extends State<SpaceNews> {
                       borderRadius: BorderRadius.circular(30.0), //<-- SEE HERE
                     ),
                     child: FrostedGlassBox(
-                      theHeight: 100,
+                      theHeight: 150,
                       theWidth: 100,
                       theChild: ListTile(
                         leading: SizedBox(
@@ -98,7 +87,15 @@ class _SpaceNewsState extends State<SpaceNews> {
                         ),
                         title: Text(
                           title!,
-                          style: const TextStyle(color: Colors.white),
+                          style: const TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        subtitle: Text(
+                          description!,
+                          style: const TextStyle(
+                            color: Colors.grey,
+                          ),
                         ),
                       ),
                     ),
