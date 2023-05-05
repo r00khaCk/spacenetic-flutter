6b48bc71bed52e981c1497a4b07677fa899510d1
wenfayeee <munnfayee@gmail.com>
2023-05-05 09:03:39 +0800

    Fixed home route

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const MyHomeSwipeView(),
+      home: const SplashscreenPage(),
       routes: {
         '/main': (context) => const SplashscreenPage(),
         '/home': (context) => const MyHomeSwipeView(),
